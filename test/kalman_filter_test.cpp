8b3e2a1b177ebf140cf66e4304634f6c8d629bdc
Samuele Sandrini <samuele.sandrini@polito.it>
2024-08-07 10:31:02 +0200

    Added some tests

--- test/kalman_filter_test.cpp
+++ test/kalman_filter_test.cpp
@@ -378,6 +378,145 @@ TEST(KalmanFilterTest, KalmanFilterAndParamPlugin)
   ASSERT_TRUE(kf->get_P0().isApprox(expected_initial_state_covariance));
 }
 
+TEST(KalmanFilterTest, KalmanFilterAndParamWrongPlugin)
+{
+  // Set expected parameter values
+  int state_size = 3;
+  int input_size = 2;
+  int output_size = 1;
+
+  std::vector<double> flat_A, flat_B, flat_C, flat_D, flat_initial_state;
+  std::vector<double> flat_L;
+  std::vector<double> flat_measurement_noise_covariance,
+    flat_process_noise_covariance,
+    flat_initial_state_covariance;
+
+  Eigen::MatrixXd expected_A(state_size, state_size);
+  expected_A << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+
+  Eigen::MatrixXd expected_B(state_size, input_size);
+  expected_B << 10, 11, 12, 13, 14, 14;
+
+  Eigen::MatrixXd expected_C(output_size, state_size);
+  expected_C << 14, 15, 16;
+
+  Eigen::MatrixXd expected_D(output_size, input_size);
+  expected_D << 17, 18;
+
+  Eigen::VectorXd expected_initial_state(state_size);
+  expected_initial_state << 19, 20, 21;
+
+  for (int i = 0; i < expected_A.size(); ++i) {
+    flat_A.push_back(expected_A.data()[i]);
+  }
+
+  for (int i = 0; i < expected_B.size(); ++i) {
+    flat_B.push_back(expected_B.data()[i]);
+  }
+
+  for (int i = 0; i < expected_C.size(); ++i) {
+    flat_C.push_back(expected_C.data()[i]);
+  }
+
+  for (int i = 0; i < expected_D.size(); ++i) {
+    flat_D.push_back(expected_D.data()[i]);
+  }
+
+  for (int i = 0; i < expected_initial_state.size(); ++i) {
+    flat_initial_state.push_back(expected_initial_state.data()[i]);
+  }
+
+  Eigen::MatrixXd expected_L_gain(state_size, output_size);
+  expected_L_gain << 5, 2, 9;
+
+  for (int i = 0; i < expected_L_gain.size(); ++i) {
+    flat_L.push_back(expected_L_gain.data()[i]);
+  }
+
+  Eigen::MatrixXd expected_process_noise_covariance(state_size, state_size);
+  expected_process_noise_covariance << 10, 0.1, 0.1, 0.1, 10, 0.1, 0.1, 0.1, 10;
+
+  for (int i = 0; i < expected_process_noise_covariance.size(); ++i) {
+    flat_process_noise_covariance.push_back(expected_process_noise_covariance.data()[i]);
+  }
+
+  Eigen::MatrixXd expected_measurement_noise_covariance(output_size, output_size);
+  expected_measurement_noise_covariance << 3;
+
+  for (int i = 0; i < expected_measurement_noise_covariance.size(); ++i) {
+    flat_measurement_noise_covariance.push_back(expected_measurement_noise_covariance.data()[i]);
+  }
+
+  Eigen::MatrixXd expected_initial_state_covariance(state_size, state_size);
+  expected_initial_state_covariance << 2, 0.1, 0.1, 0.1, 2, 0.1, 0.1, 0.1, 2;
+
+  for (int i = 0; i < expected_initial_state_covariance.size(); ++i) {
+    flat_initial_state_covariance.push_back(expected_initial_state_covariance.data()[i]);
+  }
+
+  std::shared_ptr<pluginlib::ClassLoader<state_observer::StateObserver>> state_observer_loader;
+  state_observer_loader.reset(
+    new pluginlib::ClassLoader<state_observer::StateObserver>(
+      "state_observers",
+      "state_observer::StateObserver"));
+  std::shared_ptr<state_observer::StateObserver> state_observer;
+
+  try {
+    state_observer =
+      state_observer_loader->createSharedInstance(
+      "state_observer::KalmanFilter");
+    std::cerr << "state_observer::KalmanFilter" << std::endl;
+  } catch (pluginlib::PluginlibException & ex) {
+    std::cerr << "The plugin failed to load for some reason. Error: " << ex.what() << std::endl;
+  }
+
+  std::shared_ptr<pluginlib::ClassLoader<state_observer::StateObserverParam>>
+  state_observer_param_loader;
+  state_observer_param_loader.reset(
+    new pluginlib::ClassLoader<state_observer::StateObserverParam>(
+      "state_observers",
+      "state_observer::StateObserverParam"));
+  std::shared_ptr<state_observer::StateObserverParam> wrong_state_observer_param;
+
+  try {
+    wrong_state_observer_param =
+      state_observer_param_loader->createSharedInstance(
+      "state_observer::LuenbergerParam");
+    std::cerr << "state_observer::KalmanFilterParam" << std::endl;
+  } catch (pluginlib::PluginlibException & ex) {
+    std::cerr << "The plugin failed to load for some reason. Error: " << ex.what() << std::endl;
+  }
+
+  rclcpp::NodeOptions node_options;
+  node_options.allow_undeclared_parameters();
+
+  node_options.append_parameter_override("state_size", 3);
+  node_options.append_parameter_override("input_size", 2);
+  node_options.append_parameter_override("output_size", 1);
+  node_options.append_parameter_override("state_transition_matrix", flat_A);
+  node_options.append_parameter_override("input_matrix", flat_B);
+  node_options.append_parameter_override("output_matrix", flat_C);
+  node_options.append_parameter_override("feedforward_matrix", flat_D);
+  node_options.append_parameter_override("initial_state", flat_initial_state);
+  node_options.append_parameter_override("Luenberger_gain", flat_L);
+  node_options.append_parameter_override("process_noise_covariance", flat_process_noise_covariance);
+  node_options.append_parameter_override(
+    "measurement_noise_covariance",
+    flat_measurement_noise_covariance);
+  node_options.append_parameter_override("initial_state_covariance", flat_initial_state_covariance);
+
+  rclcpp_lifecycle::LifecycleNode::SharedPtr node = rclcpp_lifecycle::LifecycleNode::make_shared(
+    "kalman_filter_test_node", node_options);
+
+  wrong_state_observer_param->initialize(node);
+
+  EXPECT_THROW(
+  {
+    state_observer->set_parameters(
+      wrong_state_observer_param);
+  }, std::invalid_argument);
+}
+
 // TEST(KalmanFilterTest, Ros2PublisherIntegration) {
 //   // Define system model matrices
 //   double dt = 0.1; // Sample time
